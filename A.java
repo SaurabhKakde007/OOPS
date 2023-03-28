7842a5f5bab6282805295660e4758df23d3ca6a7
saurabh kakde <127628985+SaurabhKakde007@users.noreply.github.com>
2023-03-29 00:09:29 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,9 +1,11 @@
+package p1;
 
 public class A {
-	int x=399;
-
-
-public void test() {
-	System.out.println(80);
-}
+        //Defination--If we develope a feature 
+	//such that it can be take more 
+	//than one form depending on suction.
+	
+	//There are two ways to achived polymorphism
+	//1)Overloading
+	//2)overriding
 }
