01067dc07794757369b683ec36ed9da44e02f76b
saurabh kakde <127628985+SaurabhKakde007@users.noreply.github.com>
2023-03-29 23:37:24 +0530

    Add files via upload

---
+++ Overloading main-Polymorphism/src/p1/A.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class A {
+	public void test1() {
+		System.out.println(33);
+	}
+	public void test2() {
+		System.out.println(50);
+	}
+	public static void main(String[] args) {
+		A a1=new A();
+		a1.test1();
+		a1.test2(); 
+	}
+
+}
---
+++ Overloading main-Polymorphism/src/p2/A.java
@@ -0,0 +1,13 @@
+package p2;
+
+public class A {
+	public static void main(String[] args) {
+		
+		A.main();
+	
+	}
+	public static void main() {
+		System.out.println(333);
+	}
+
+}
